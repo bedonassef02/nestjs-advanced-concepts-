a1cdd3d842cd8e19c7d354d599bbe5a0aa2ee0c8
Bedo <bedonassef71@gmail.com>
2024-10-20 18:10:12 +0300

    worker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees/coffees.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { CronModule } from './cron/cron.module';
+import { FibonacciModule } from './fibonacci/fibonacci.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CoffeesModule, SchedulerModule, CronModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    // SchedulerModule, CronModule,
+    FibonacciModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/fibonacci/fibonacci.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { FibonacciWorkerHost } from './fibonacci-worker.host';
+
+@Controller('fibonacci')
+export class FibonacciController {
+  constructor(private readonly fibonacciWorkerHost: FibonacciWorkerHost) {}
+  @Get()
+  fibonacci(@Query('n') n: number = 10) {
+    return this.fibonacciWorkerHost.run(n);
+  }
+}
