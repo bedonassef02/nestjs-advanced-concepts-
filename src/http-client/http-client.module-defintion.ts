cd06de378dbb59cf853b73be2321844b96ff8ab1
Bedo <bedonassef71@gmail.com>
2024-10-20 19:44:03 +0300

    Building_Configurable_Modules

--- src/http-client/http-client.module-defintion.ts
+++ src/http-client/http-client.module-defintion.ts
@@ -1,11 +1,23 @@
 import { ConfigurableModuleBuilder } from '@nestjs/common';
+import e from 'express';
 
 export const {
   ConfigurableModuleClass,
   MODULE_OPTIONS_TOKEN: HTTP_MODULE_OPTIONS,
+//   OPTIONS_TYPE,
+//   ASYNC_OPTIONS_TYPE
 } = new ConfigurableModuleBuilder<{
   baseUrl: string;
-}>()
-  .setClassMethodName('forRoot')
-  .setFactoryMethodName('resolve')
+}>({ alwaysTransient: true })
+  //   .setClassMethodName('forRoot')
+  //   .setFactoryMethodName('resolve')
+  .setExtras<{ isGlobal: boolean }>(
+    {
+      isGlobal: true,
+    },
+    (definition, extras) => ({
+      ...definition,
+      global: extras.isGlobal,
+    }),
+  )
   .build();
